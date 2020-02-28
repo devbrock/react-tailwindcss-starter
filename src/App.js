4c050653ca4196c9523ede6548c53fd2a7711d0e
Brock Balducci <tbrockb@gmail.com>
2020-02-28 14:47:10 -0600

    starter for react projects with tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="max-w-sm p-8 bg-gray-200 text-center mx-auto shadow-lg">
+				<img src={logo} className="" alt="logo" />
+				<p className="text-gray-900 font-display">
+					Edit src/App.js and save to reload.
+				</p>
+				<a
+					className="text-gray-900 underline"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	theme: {
+		extend: {
+			fontFamily: {
+				display: ['Montserrat'],
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};

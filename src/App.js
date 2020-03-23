a4165fcedf42bb1c751cfa07f13772dd827bd8c8
DevBrock <tbrockb@gmail.com>
2020-03-22 20:54:40 -0500

    change some stuff that messed up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
-import PostsList from './components/PostsList';
-import Header from './components/layout/Header';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Post from './components/Post';
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
-				<Header />
-				<Route path="/" exact component={PostsList} />
-				<Route path="/:id" exact component={Post} />
+				<h1 className="text-5xl text-blue-900 mt-16 font-bold text-center">
+					🔥 Hello from React + TailwindCSS 🔥{' '}
+				</h1>
 			</div>
 		</Router>
 	);
